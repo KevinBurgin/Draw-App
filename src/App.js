48a3ffc4090acfd76793bfc39f1efafa83ecee0c
Kevin Burgin <Kevinburgin33@gmail.com>
2023-01-31 20:13:50 -0800

    updated ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect, useCallback} from 'react'
-import { useNavigate} from 'react-router-dom'
+// import { useNavigate} from 'react-router-dom'
 import './App.css';
 
 
@@ -53,45 +53,19 @@ const brush =[
     "100",
     "500"
 ]
-const radius =[
-  "1",
-  "2",
-  "3",
-  "4",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "15",
-  "20",
-  "25",
-  "30",
-  "35",
-  "40",
-  "45",
-  "50",
-  "55",
-  "60",
-  "65",
-  "70",
-  "75",
-  "80", 
-  "85", 
-  "90",
-  "95",
-  "100",
-  "500"
-]
+// const fill =[
+//   " Blue",
+//   " Red"
+// ]
 
 function App() {
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const canvasRef = useRef(null)
     const contextRef = useRef(null)
 
 
-    const [selectedRadius, setSelectedRadius] = useState(radius[0])
+    // const [selectedFill, setSelectedFill] = useState(fill[0])
     const [selectedBrush, setSelectedBrush] = useState(brush[0])
     const [selectedColor, setSelectedColor] = useState(colors[0])
     const [mouseDown, setMouseDown] = useState(false);
@@ -109,9 +83,9 @@ function App() {
    const draw = useCallback((x, y) => {
        if (mouseDown) {
         contextRef.current.beginPath();
+      
         contextRef.current.strokeStyle = selectedColor;
         contextRef.current.lineWidth = selectedBrush;
-        contextRef.current.radius = selectedRadius;
         contextRef.current.lineJoin = "round";
         contextRef.current.moveTo(lastPosition.x, lastPosition.y)
         contextRef.current.lineTo(x, y)
@@ -139,9 +113,56 @@ function App() {
        contextRef.current.clearRect(0, 0, contextRef.current.canvas.width, contextRef.current.canvas.height)
    }
 
-   const redirect = () => {
-    navigate.push('/')
-    }  
+   const save = () => {
+    contextRef.current.save(0,0,contextRef.current.canvas.width, contextRef.current.canvas.height)
+   }
+
+   const restore = () => {
+    contextRef.current.restore(0,0,contextRef.current.canvas.width, contextRef.current.canvas.height)
+   }
+
+  //  const fill = () => {
+  //     // contextRef.current.fillRect(10,10,100,100);
+  //     for (let i = 0; i < 6; i++) {
+  //       for (let j = 0; j < 6; j++) {
+  //         contextRef.current.fillStyle = `rgb(
+  //             ${Math.floor(355 - 42.5 * i)},
+  //             ${Math.floor(355 - 42.5 * j)},
+  //             0)`;
+  //         contextRef.current.fillRect(j * 25, i * 25, 25, 25);
+  //       }
+  //     }
+  //  }
+
+    // const fill = useCallback((x,y) => {
+    //   let start = { x: 50,    y: 20  };
+    //   let cp1 =   { x: 230,   y: 30  };
+    //   let cp2 =   { x: 150,   y: 80  };
+    //   let end =   { x: 250,   y: 100 };
+            
+    //   canvasRef.current.beginPath();
+    //   canvasRef.current.moveTo(start.x, start.y);
+    //   canvasRef.current.bezierCurveTo(cp1.x, cp1.y, cp2.x, cp2.y, end.x, end.y);
+    //   canvasRef.current.stroke();
+
+    //   // Start and end points
+    //   canvasRef.current.fillStyle = 'blue';
+    //   canvasRef.current.beginPath();
+    //   canvasRef.current.arc(start.x, start.y, 5, 0, 2 * Math.PI);  // Start point
+    //   canvasRef.current.arc(end.x, end.y, 5, 0, 2 * Math.PI);      // End point
+    //   canvasRef.current.fill();
+
+    //   // Control points
+    //   canvasRef.current.fillStyle = 'red';
+    //   canvasRef.current.beginPath();
+    //   canvasRef.current.arc(cp1.x, cp1.y, 5, 0, 2 * Math.PI);  // Control point one
+    //   canvasRef.current.arc(cp2.x, cp2.y, 5, 0, 2 * Math.PI);  // Control point two
+    //   canvasRef.current.fill();
+    // }
+
+  //  const redirect = () => {
+  //   navigate.push('/')
+  //   }  
     
    const onMouseDown = (e) => {
        setPosition({
@@ -162,8 +183,8 @@ function App() {
     return (
         <div className="draw-container">
             {/* <NavBar/> */}
-            <canvas
-            style ={{borderBottom: "5px solid black"}}
+            <canvas className='canvas'
+            style ={{border: "5px solid black"}}
             width={1440}
             height={700}
             ref={canvasRef}
@@ -188,16 +209,17 @@ function App() {
                     )
                 }
             </select>
-            <select value={selectedRadius} className="color-select" onChange={(e) => setSelectedRadius(e.target.value)}>
+            {/* <select value={selectedFill} className="color-select" onChange={(e) => setSelectedFill(e.target.value)}>
                 {
-                    brush.map(
-                        brush => <option key={radius} value={radius}>Brush Radius:{radius}</option>
+                    fill.map(
+                        fill => <option key={fill} value={fill}>Fill:{fill}</option>
                     )
                 }
-                </select>
+                </select> */}
                 <button onClick={clear} className="clear-button">Clear</button>
                 <button onClick={download} className="download-button">Download</button>
-                <button onClick={redirect} className="home-button">Home</button>
+                <button onClick={save} className="save-button">Save</button>
+                <button onClick={restore} className="restore-button">Restore</button>
             </div>
         </div>
     )
